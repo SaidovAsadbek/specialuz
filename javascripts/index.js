7f31724305875655064eeaa5b2ee67b488885df0
SaidovAsadbek <asadbeksaidov011@gmail.com>
2022-07-01 16:10:47 +0500

    end header

---
+++ javascripts/index.js
@@ -0,0 +1,8 @@
+// Nav , toggle modal
+create.onclick = function(){
+    modalPage.classList.toggle('active')
+}
+modalPage.onclick = function(){
+    modalPage.classList.toggle('active')
+}
+// Nav , toggle modal 
