6d5076fba014ecc26bb6b2edd550e30ab965c96a
SaidovAsadbek <asadbeksaidov011@gmail.com>
2022-07-02 20:10:01 +0500

    website media and main card

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,4 +5,14 @@ window.addEventListener("load", () => {
     $id("sp-submenu").classList.toggle("hide-menu");
     $id("chevronDown").classList.toggle("chevron-down");
   });
+  ("use strict");
+
+  document.querySelector(".toggle").addEventListener("click", function () {
+    this.classList.toggle("activate");
+  });
+
+  $id("nav-toggle-submit").addEventListener("click", () => {
+    $id("nav-toggle-menu").classList.toggle("hide-toggle");
+  });
+  ("use strict");
 });
