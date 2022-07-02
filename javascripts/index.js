4f369abcf0997176b38dc837e98aafc460facf12
SaidovAsadbek <asadbeksaidov011@gmail.com>
2022-07-02 13:05:57 +0500

    media

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,3 +1,9 @@
+// call center Toggle
+sp_call_btn.onclick = function(){
+    collaps.classList.toggle('active')
+}
+// call center Toggle
+
 // Nav , toggle modal
 create.onclick = function(){
     modalPage.classList.toggle('active')
