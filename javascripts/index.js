8aa9f21cee26db803be0ad46d265f25e8330f32b
SaidovAsadbek <asadbeksaidov011@gmail.com>
2022-07-02 19:42:52 +0500

    media

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,14 +1,24 @@
 // call center Toggle
-sp_call_btn.onclick = function(){
-    collaps.classList.toggle('active')
-}
+sp_call_btn.onclick = function () {
+  collaps.classList.toggle("active");
+  sp_bar.classList.remove('active')
+};
 // call center Toggle
+// Nav toggle 
+sp_bars_btn.onclick = function(){
+  this.classList.toggle('active')
+  sp_bar.classList.toggle('active')
+  collaps.classList.remove("active");
+}
+// Nav toggle 
 
 // Nav , toggle modal
-create.onclick = function(){
-    modalPage.classList.toggle('active')
+for (let i = 0; i < create.length; i++) {
+  create[i].onclick = function () {
+    modalPage.classList.toggle("active");
+  };
 }
-modalPage.onclick = function(){
-    modalPage.classList.toggle('active')
-}
-// Nav , toggle modal 
+modalPage.onclick = function () {
+  modalPage.classList.toggle("active");
+};
+// Nav , toggle modal
