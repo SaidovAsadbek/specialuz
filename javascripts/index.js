378861e2bd0b26ccbdd1fb195bbbf26c90498d0a
SaidovAsadbek <asadbeksaidov011@gmail.com>
2022-07-02 20:09:08 +0500

    heder,section end

---
+++ javascripts/index.js
@@ -0,0 +1,14 @@
+const modalBtn = document.querySelector('.modal-btn')
+const modalPage = document.querySelector('.modal')
+
+modalBtn.onclick = ()=>{
+    modalPage.classList.toggle('active')
+    
+}
+
+
+
+
+
+
+
