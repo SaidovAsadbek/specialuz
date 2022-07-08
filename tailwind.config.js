856916b1a69ce359cb53840422364ec1ed8ddf96
SaidovAsadbek <asadbeksaidov011@gmail.com>
2022-07-08 16:35:33 +0500

    tailwind test success

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors:{
+      'lgreen' : '#00e200',
+      'lgreen2': '#0ebd0e',
+      'dgreen': '#008d00',
+      'white' : '#fff'
+    },
+    extend: {},
+  },
+  plugins: [],
+};
